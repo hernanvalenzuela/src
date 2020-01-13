667eed59f2755ee7fd8c0054752c94c1d83b2d82
Hernan Valenzuela <hernanvalenzuela77@gmail.com>
2020-01-13 15:16:35 -0300

    scrollviewer issue solved

--- Reddit/MainPage.xaml.cs
+++ Reddit/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
@@ -10,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -18,6 +20,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
+using Reddit.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,15 +29,49 @@ namespace Reddit
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
     public sealed partial class MainPage : Page
     {
+        public ObservableCollection<ItemReddit> CollectionItemsReddit { get; set; }
         public MainPage()
         {
             this.InitializeComponent();
             var task = Task.Run(() => TryPostJsonAsync());
             task.Wait();
+            // ReadData();
         }
 
+        private void ReadData()
+        {
+
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            var headers = httpClient.DefaultRequestHeaders;
+            Uri requestUri = new Uri("https://www.reddit.com//top/.json?count=20");
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+            try
+            {
+                //Send the GET request
+                httpResponse = httpClient.GetAsync(requestUri).GetResults();
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    httpResponseBody = httpResponse.Content.ReadAsStringAsync().GetResults();
+
+                    var obj = JsonConvert.DeserializeObject<Model.RootObject>(httpResponseBody, new JsonSerializerSettings() { Culture = CultureInfo.CurrentCulture });
+                    var list = (from r in obj.data.children
+                                select new { ImgUrl = r.data.thumbnail,
+                                    Title = r.data.title,
+                                    Author = r.data.author,
+                                    NumberOfComments = r.data.num_comments
+                                }).ToList().Take(1);
+                    gvthumbails.ItemsSource = list;
+                }
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+        }
         private async Task TryPostJsonAsync()
         {
             Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
@@ -50,13 +87,21 @@ namespace Reddit
                 httpResponse.EnsureSuccessStatusCode();
                 httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
                 var obj = JsonConvert.DeserializeObject<Model.RootObject>(httpResponseBody, new JsonSerializerSettings() { Culture = CultureInfo.CurrentCulture });
-                var list = (from r in obj.data.children
-                            select new { ImgUrl = r.data.thumbnail }).ToList();
+                CollectionItemsReddit = new ObservableCollection<ItemReddit>( (from r in obj.data.children
+                            select new ItemReddit { ImgUrl = r.data.thumbnail,
+                                Title = r.data.title,
+                                Author = r.data.author,
+                                NumberOfComments = r.data.num_comments
+                            }).ToList());
 
-                await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-                    {
-                        gvthumbails.ItemsSource = list;
-                    });
+
+               // gvthumbails.ItemsSource = CollectionItemsReddit;
+
+                //await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                // {
+                //     gvthumbails.ItemsSource = list;
+                // }
+                // );
             }
             catch (Exception ex)
             {
---
+++ Reddit/Model/ItemReddit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reddit.Model
+{
+    public class ItemReddit
+    {
+        public string ImgUrl { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double EntryDate { get; set; }
+        public int NumberOfComments { get; set; }
+
+        public string Status { get; set; }
+
+    }
+}
